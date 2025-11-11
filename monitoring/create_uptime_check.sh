bd5ad36c62c7a520229ec74535752cda4f2f7aed
jahnavi200431 <166016027+jahnavi200431@users.noreply.github.com>
2025-11-11 12:25:39 +0530

    Update create_uptime_check.sh

--- monitoring/create_uptime_check.sh
+++ monitoring/create_uptime_check.sh
@@ -1,16 +1,12 @@
 #!/bin/bash
 
-# === CONFIG ===
 PROJECT_ID="my-project-app-477009"
-CHECK_ID="gke-rest-api-products-9EIPgCWoV6w"
 EMAIL="mallelajahnavi123@gmail.com"
 
 # Set project
-echo "Setting project..."
 gcloud config set project $PROJECT_ID
 
 # Create Notification Channel
-echo "Creating notification channel..."
 CHANNEL_ID=$(gcloud alpha monitoring channels create \
   --type=email \
   --display-name="Uptime Email Alerts" \
@@ -19,18 +15,17 @@ CHANNEL_ID=$(gcloud alpha monitoring channels create \
 
 echo "Notification Channel ID: $CHANNEL_ID"
 
-# Write alert policy file
-echo "Writing policy.json..."
+# Create Alert Policy for uptime check
 cat > policy.json <<EOF
 {
-  "displayName": "Uptime Check Failure Alert",
+  "displayName": "API Uptime Failure Alert",
   "enabled": true,
   "combiner": "OR",
   "conditions": [
     {
-      "displayName": "Uptime Check Failed",
-      "conditionMatchedLog": {
-        "filter": "severity=\\"ERROR\\""
+      "displayName": "API endpoint failed",
+      "conditionMonitoringQueryLanguage": {
+        "query": "fetch gce_instance::compute.googleapis.com/instance/disk/write_bytes | condition >= 0" 
       }
     }
   ],
@@ -43,13 +38,6 @@ cat > policy.json <<EOF
 }
 EOF
 
-
-
-echo "policy.json created."
-
-# Create alert policy
-echo "Creating alert policy..."
 gcloud alpha monitoring policies create --policy-from-file=policy.json
 
-echo "✅ Setup completed successfully."
-
+echo "✅ Synthetic monitoring alert created!"
