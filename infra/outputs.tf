6feece35b9d84496ec17197dd09b19e869c76c66
jahnavi200431 <166016027+jahnavi200431@users.noreply.github.com>
2025-11-13 09:52:55 +0530

    Delete infra directory

--- infra/outputs.tf
+++
@@ -1,9 +0,0 @@
-output "db_public_ip" {
-  value = google_sql_database_instance.postgres_instance.public_ip_address
-}
-output "instance_connection_name" {
-  value = google_sql_database_instance.postgres_instance.connection_name
-}
-output "gke_cluster" {
-  value = google_container_cluster.gke_cluster.name
-}
--- infra/terraform.tfvars
+++
@@ -1,4 +0,0 @@
-project_id   = "my-project-app-477009"
-region       = "us-central1"
-zone         = "us-central1-a"
-db_password  = "postgres"   # change to a strong password for production
