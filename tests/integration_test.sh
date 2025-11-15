d506543e8cfb30c295551362176314091fcb2bb2
jahnavi200431 <166016027+jahnavi200431@users.noreply.github.com>
2025-11-15 18:52:16 +0530

    Update integration_test.sh

--- tests/integration_test.sh
+++ tests/integration_test.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 set -euo pipefail
 
-# Cloud Build passes LB via environment variable
+# Cloud Build passes LB via environment variabl
 if [ -z "${LB:-}" ]; then
   echo "ERROR: LB environment variable not set."
   exit 2
