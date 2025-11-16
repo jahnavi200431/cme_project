f702f2aa123fd8a2c43deb088e045bf249388e7f
jahnavi200431 <166016027+jahnavi200431@users.noreply.github.com>
2025-11-16 17:40:05 +0530

    Update outputs.tf

--- infra1/outputs.tf
+++ infra1/outputs.tf
@@ -7,3 +7,13 @@ output "postgres_ip" {
   description = "Public IP of the PostgreSQL instance"
   value       = google_sql_database_instance.postgres.public_ip_address
 }
+
+output "gke_node_service_account" {
+  description = "GKE Node Service Account email"
+  value       = google_service_account.gke_nodes.email
+}
+
+output "gke_app_service_account" {
+  description = "GKE App Workload Identity Service Account email"
+  value       = google_service_account.app_sa.email
+}
