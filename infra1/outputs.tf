8b5eaf24ab77dde68a2a82c1a390dd2c5440d027
jahnavi200431 <166016027+jahnavi200431@users.noreply.github.com>
2025-11-16 18:08:42 +0530

    Update outputs.tf

--- infra1/outputs.tf
+++ infra1/outputs.tf
@@ -3,17 +3,12 @@ output "gke_endpoint" {
   value       = google_container_cluster.gke.endpoint
 }
 
-output "postgres_ip" {
-  description = "Public IP of the PostgreSQL instance"
+output "postgres_public_ip" {
+  description = "Public IP address of the PostgreSQL instance"
   value       = google_sql_database_instance.postgres.public_ip_address
 }
 
-output "gke_node_service_account" {
-  description = "GKE Node Service Account email"
-  value       = google_service_account.gke_nodes.email
-}
-
-output "gke_app_service_account" {
-  description = "GKE App Workload Identity Service Account email"
-  value       = google_service_account.app_sa.email
+output "node_service_account" {
+  description = "Service account used by the GKE node pool"
+  value       = local.node_sa
 }
