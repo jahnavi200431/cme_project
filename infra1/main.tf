d0b6e4291f3e2354861827706f6c22dd522efabf
jahnavi200431 <166016027+jahnavi200431@users.noreply.github.com>
2025-11-17 20:44:07 +0530

    Update main.tf

--- infra1/main.tf
+++ infra1/main.tf
@@ -37,12 +37,12 @@ resource "google_project_iam_member" "cloudsql_client" {
 #  SECURE VPC & SUBNET FOR GKE
 # -------------------------------------------------------------
 resource "google_compute_network" "gke_vpc" {
-  name                    = "gke-secure-vpc2"
+  name                    = "gke-secure-vpc3"
   auto_create_subnetworks = false
 }
 
 resource "google_compute_subnetwork" "gke_subnet" {
-  name          = "gke-secure-subnet2"
+  name          = "gke-secure-subnet3"
   region        = var.region
   network       = google_compute_network.gke_vpc.self_link
   ip_cidr_range = "10.50.0.0/20"
