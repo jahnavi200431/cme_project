cbbc4704fb33a500411cf0398f441c104d2b8df3
jahnavi200431 <166016027+jahnavi200431@users.noreply.github.com>
2025-11-18 13:37:23 +0530

    Update main.tf

--- infra1/main.tf
+++ infra1/main.tf
@@ -99,8 +99,8 @@ resource "google_compute_router" "nat_router1" {
 
 resource "google_compute_router_nat" "nat_config1" {
   name                              = "nat-config1"
-  router                            = google_compute_router.nat_router.name
-  region                            = google_compute_router.nat_router.region
+  router                            = google_compute_router.nat_router1.name
+  region                            = google_compute_router.nat_router1.region
   nat_ip_allocate_option             = "AUTO_ONLY"
   source_subnetwork_ip_ranges_to_nat = "ALL_SUBNETWORKS_ALL_IP_RANGES"
 }
