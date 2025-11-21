1b63a41c782be99f74ba2158a2192f7415c46e57
lakshmi13520 <lakshmi13520@gmail.cim>
2025-11-21 21:18:35 +0530

    vpc connection

--- infra/main.tf
+++ infra/main.tf
@@ -33,13 +33,9 @@ resource "google_compute_subnetwork" "private_subnet" {
 # GKE Cluster (Create if not already present)
 # ------------------------------------------------------------
 # Data source to check if the GKE cluster exists
-data "google_container_cluster" "cluster" {
-  name     = var.cluster_name
-  location = var.zone
-}
 
 resource "google_container_cluster" "cluster" {
-  count                  = length(google_compute_network.vpc_network) > 0 && length(data.google_container_cluster.cluster) == 0 ? 1 : 0
+  count                  = length(google_compute_network.vpc_network) > 0 ? 1 : 0
   name                   = var.cluster_name
   location               = var.zone
   deletion_protection    = false
