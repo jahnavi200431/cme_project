9fcced174b89e50485aea21f67f3d78fc8f1bcc0
lakshmi13520 <lakshmi13520@gmail.cim>
2025-11-21 21:57:44 +0530

    vpc connection

--- infra/main.tf
+++ infra/main.tf
@@ -4,36 +4,39 @@ provider "google" {
   zone    = var.zone
 }
 
-# ------------------------------------------------------------
-# VPC Network (Create if it doesn't exist)
-# ------------------------------------------------------------
-
+# VPC Network
 resource "google_compute_network" "vpc_network" {
   name                   = var.vpc_name
   auto_create_subnetworks = false
 }
 
-# ------------------------------------------------------------
 # Private Subnet in the VPC
-# ------------------------------------------------------------
 resource "google_compute_subnetwork" "private_subnet" {
-  name          = var.subnet_name
-  region        = var.region
-  network       = google_compute_network.vpc_network.name
-  ip_cidr_range = "10.0.0.0/24"
-  depends_on    = [google_compute_network.vpc_network]
+  name                     = var.subnet_name
+  region                   = var.region
+  network                  = google_compute_network.vpc_network.name
+  ip_cidr_range            = "10.0.0.0/24"
+  private_ip_google_access = true  # Enable private Google access
 }
 
-# ------------------------------------------------------------
-# GKE Cluster (Create if not already present)
-# ------------------------------------------------------------
-# Data source to check if the GKE cluster exists
+# Private IP Address for Cloud SQL
+resource "google_compute_global_address" "private_ip_address" {
+  name         = "private-ip-address"
+  purpose      = "VPC_PEERING"
+  address_type = "INTERNAL"
+  network      = google_compute_network.vpc_network.name
+  subnetwork   = google_compute_subnetwork.private_subnet.name
+  region       = var.region
+}
 
+# GKE Cluster (Create if not already present)
 resource "google_container_cluster" "cluster" {
   name                     = var.cluster_name
   location                 = var.zone
   deletion_protection      = false
   remove_default_node_pool = true
+
+  initial_node_count       = 1  # Ensure at least 1 node
 
   network    = google_compute_network.vpc_network.name
   subnetwork = google_compute_subnetwork.private_subnet.name
@@ -48,7 +51,7 @@ resource "google_container_cluster" "cluster" {
   depends_on = [google_compute_subnetwork.private_subnet]
 }
 
-## Create the node pool (which Terraform manages)
+# Node Pool for GKE Cluster
 resource "google_container_node_pool" "node_pool" {
   cluster   = google_container_cluster.cluster.name
   location  = var.zone
@@ -56,16 +59,13 @@ resource "google_container_node_pool" "node_pool" {
 
   node_config {
     machine_type = "e2-micro"
-    oauth_scopes = [
-      "https://www.googleapis.com/auth/logging.write",
-           "https://www.googleapis.com/auth/monitoring",
-           "https://www.googleapis.com/auth/cloud-platform"
-    ]
+    oauth_scopes = ["https://www.googleapis.com/auth/cloud-platform"]
   }
+
+  depends_on = [google_container_cluster.cluster]
 }
-# ------------------------------------------------------------
-# Cloud SQL Database Instance (Create if VPC exists)
-# ------------------------------------------------------------
+
+# Cloud SQL Database Instance
 resource "google_sql_database_instance" "db_instance" {
   name            = var.db_instance_name
   database_version = "POSTGRES_13"
@@ -79,33 +79,28 @@ resource "google_sql_database_instance" "db_instance" {
     }
   }
 
-  depends_on = [google_compute_network.vpc_network]
+  depends_on = [
+    google_compute_network.vpc_network,
+    google_compute_global_address.private_ip_address
+  ]
 }
 
-# ------------------------------------------------------------
 # Fetch the password from Google Cloud Secret Manager
-# ------------------------------------------------------------
 data "google_secret_manager_secret_version" "db_password" {
-  secret = "db-password"  # Update with the correct secret name
+  secret = "db-password"
 }
 
-# ------------------------------------------------------------
 # Create Database
-# ------------------------------------------------------------
 resource "google_sql_database" "database" {
-  name            = var.db_name
-  instance        = google_sql_database_instance.db_instance.name
-  depends_on      = [google_sql_database_instance.db_instance]
+  name     = var.db_name
+  instance = google_sql_database_instance.db_instance.name
 }
 
-# ------------------------------------------------------------
 # Create DB User
-# ------------------------------------------------------------
 resource "google_sql_user" "db_user" {
-  name            = var.db_user
-  instance        = google_sql_database_instance.db_instance.name
-  password        = data.google_secret_manager_secret_version.db_password.secret_data
-  depends_on      = [google_sql_database_instance.db_instance]
+  name     = var.db_user
+  instance = google_sql_database_instance.db_instance.name
+  password = data.google_secret_manager_secret_version.db_password.secret_data
 }
 
 # ------------------------------------------------------------
