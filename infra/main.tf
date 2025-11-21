0721823b587d2c2384fe25f1088baab7889d8cba
lakshmi13520 <lakshmi13520@gmail.cim>
2025-11-21 22:03:12 +0530

    vpc connection

--- infra/main.tf
+++ infra/main.tf
@@ -4,13 +4,17 @@ provider "google" {
   zone    = var.zone
 }
 
-# VPC Network
+# ------------------------------------------------------------
+# VPC Network (Create if it doesn't exist)
+# ------------------------------------------------------------
 resource "google_compute_network" "vpc_network" {
   name                   = var.vpc_name
   auto_create_subnetworks = false
 }
 
+# ------------------------------------------------------------
 # Private Subnet in the VPC
+# ------------------------------------------------------------
 resource "google_compute_subnetwork" "private_subnet" {
   name                     = var.subnet_name
   region                   = var.region
@@ -19,17 +23,20 @@ resource "google_compute_subnetwork" "private_subnet" {
   private_ip_google_access = true  # Enable private Google access
 }
 
-# Private IP Address for Cloud SQL
-resource "google_compute_global_address" "private_ip_address" {
+# ------------------------------------------------------------
+# Reserve a Private IP Address (For Cloud SQL)
+# ------------------------------------------------------------
+resource "google_compute_address" "private_ip_address" {
   name         = "private-ip-address"
-  purpose      = "VPC_PEERING"
-  address_type = "INTERNAL"
+  address_type = "INTERNAL"  # Set the IP to internal (private IP)
   network      = google_compute_network.vpc_network.name
   subnetwork   = google_compute_subnetwork.private_subnet.name
-  region       = var.region
+  region       = var.region  # Ensure this is set to the correct region
 }
 
+# ------------------------------------------------------------
 # GKE Cluster (Create if not already present)
+# ------------------------------------------------------------
 resource "google_container_cluster" "cluster" {
   name                     = var.cluster_name
   location                 = var.zone
@@ -51,7 +58,9 @@ resource "google_container_cluster" "cluster" {
   depends_on = [google_compute_subnetwork.private_subnet]
 }
 
+# ------------------------------------------------------------
 # Node Pool for GKE Cluster
+# ------------------------------------------------------------
 resource "google_container_node_pool" "node_pool" {
   cluster   = google_container_cluster.cluster.name
   location  = var.zone
@@ -65,7 +74,9 @@ resource "google_container_node_pool" "node_pool" {
   depends_on = [google_container_cluster.cluster]
 }
 
+# ------------------------------------------------------------
 # Cloud SQL Database Instance
+# ------------------------------------------------------------
 resource "google_sql_database_instance" "db_instance" {
   name            = var.db_instance_name
   database_version = "POSTGRES_13"
@@ -81,26 +92,52 @@ resource "google_sql_database_instance" "db_instance" {
 
   depends_on = [
     google_compute_network.vpc_network,
-    google_compute_global_address.private_ip_address
+    google_compute_address.private_ip_address
   ]
 }
 
+# ------------------------------------------------------------
 # Fetch the password from Google Cloud Secret Manager
+# ------------------------------------------------------------
 data "google_secret_manager_secret_version" "db_password" {
   secret = "db-password"
 }
 
+# ------------------------------------------------------------
 # Create Database
+# ------------------------------------------------------------
 resource "google_sql_database" "database" {
   name     = var.db_name
   instance = google_sql_database_instance.db_instance.name
 }
 
+# ------------------------------------------------------------
 # Create DB User
+# ------------------------------------------------------------
 resource "google_sql_user" "db_user" {
   name     = var.db_user
   instance = google_sql_database_instance.db_instance.name
   password = data.google_secret_manager_secret_version.db_password.secret_data
+}
+
+# ------------------------------------------------------------
+# Firewall Rule (Create if VPC exists)
+# ------------------------------------------------------------
+resource "google_compute_firewall" "allow_internal" {
+  name                   = "allow-internal-traffic"
+  network                = google_compute_network.vpc_network.name
+  direction              = "INGRESS"
+  priority               = 1000
+
+  allow {
+    protocol = "tcp"
+    ports    = ["5432"]
+  }
+
+  source_ranges = ["10.0.0.0/24"]
+  target_tags   = ["gke-node"]
+
+  depends_on = [google_compute_network.vpc_network]
 }
 
 # ------------------------------------------------------------
