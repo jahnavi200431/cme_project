4350e8263f498f32fd17379111c9a4dbe82e8f42
jahnavi200431 <166016027+jahnavi200431@users.noreply.github.com>
2025-11-22 22:29:09 +0530

    Update app.py

--- app/app.py
+++ app/app.py
@@ -101,7 +101,7 @@ app.wsgi_app = RequestResponseLoggerMiddleware(app.wsgi_app)
 # ---------------------------------------------------------
 # API KEY CHECK
 # ---------------------------------------------------------
-API_KEY = os.getenv("API_KEY")
+API_KEY = "restapi123"
 
 def require_api_key():
     provided_key = (
@@ -138,11 +138,12 @@ def readiness():
 # ---------------------------------------------------------
 # DATABASE CONFIG
 # ---------------------------------------------------------
-DB_HOST = os.getenv("DB_HOST")
-DB_NAME = os.getenv("DB_NAME")
-DB_USER = os.getenv("DB_USER")
-DB_PASS = os.getenv("DB_PASS")
-DB_PORT = os.getenv("DB_PORT")
+DB_HOST = "127.0.0.1"
+DB_NAME = "postgres"
+DB_USER = "user1"
+DB_PASS = "postgres"
+DB_PORT = 5432
+
 
 def get_db_connection(check_only=False):
     try:
