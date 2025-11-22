b85fb46413b82def7af7a53eacf8518582165f7e
lakshmi13520 <lakshmi13520@gmail.cim>
2025-11-22 11:34:49 +0530

    tst

--- infra/variables.tf
+++ infra/variables.tf
@@ -6,7 +6,7 @@ variable "project_id" {
 
 variable "region" {
   description = "The region where resources will be created"
-  default     = "us-central"
+  default     = "us-central1"
 }
 
 variable "zone" {
