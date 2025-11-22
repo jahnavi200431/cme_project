c06ea198e2c830f638d02dd780f36d5fe2d7dd0d
jahnavi200431 <166016027+jahnavi200431@users.noreply.github.com>
2025-11-22 16:50:29 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -10,6 +10,19 @@ data "google_compute_network" "vpc_network" {
 data "google_compute_subnetwork" "private_subnet" {
      name                      = var.subnet_name
     }
+resource "google_compute_global_address" "private_ip_range" {
+  name          = "private-ip-range"
+  purpose       = "VPC_PEERING"
+  address_type  = "INTERNAL"
+  prefix_length = 16
+  network       = data.google_compute_network.vpc_network.self_link
+}
+resource "google_service_networking_connection" "private_service_connect" {
+  network                 = data.google_compute_network.vpc_network.self_link
+  service                 = "services/servicenetworking.googleapis.com"
+  reserved_peering_ranges = [google_compute_global_address.private_ip_range.name]
+}
+
 /*
 resource "google_compute_network" "vpc_network" {
   name                   = var.vpc_name
