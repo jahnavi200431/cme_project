56b5c965d1d19c3ca3ae0f13e25725d6ef89f4cf
jahnavi200431 <166016027+jahnavi200431@users.noreply.github.com>
2025-11-23 17:37:21 +0530

    Update outputs.tf

--- infra/outputs.tf
+++ infra/outputs.tf
@@ -1,22 +1,20 @@
-/*
 output "gke_endpoint" {
-  value = google_container_cluster.gke.endpoint  # Direct reference without count.index
+  value = google_container_cluster.cluster.endpoint
 }
 
-output "db_instance_name" {
-  value = google_sql_database_instance.postgres.name  # Direct reference without count.index
+output "db_instance" {
+  value = google_sql_database_instance.db_instance.name
 }
- */
 
-/*
 output "db_user" {
   description = "The username for the PostgreSQL database"
   value       = google_sql_user.db_user.name
 }
- */
+
+ 
 
 output "db_password" {
     description = "The password for the PostgreSQL database"
   value     = data.google_secret_manager_secret_version.db_password.secret_data
-  sensitive = true  # Hide sensitive information
+  sensitive = true  
 }
