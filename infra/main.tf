26c19a4850633c32cdeb27a186849c22644c4a9d
jahnavi200431 <166016027+jahnavi200431@users.noreply.github.com>
2025-11-24 08:26:35 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -3,13 +3,16 @@ provider "google" {
   region  = var.region_name
 }
 
-
 data "google_compute_network" "vpc_network" {
-  name                   = var.vpc_name
+  name = var.vpc_name
 }
+
 data "google_compute_subnetwork" "private_subnet" {
-     name                      = var.subnet_name
-    }
+  name   = var.subnet_name
+  region = var.region_name
+}
+
+# ----------- PRIVATE SERVICE CONNECT (Cloud SQL) -----------------
 
 resource "google_compute_global_address" "private_ip_range" {
   name          = "private-ip-range1"
@@ -18,65 +21,37 @@ resource "google_compute_global_address" "private_ip_range" {
   prefix_length = 16
   network       = data.google_compute_network.vpc_network.self_link
 }
+
 resource "google_service_networking_connection" "private_service_connect" {
   network                 = data.google_compute_network.vpc_network.self_link
   service                 = "services/servicenetworking.googleapis.com"
   reserved_peering_ranges = [google_compute_global_address.private_ip_range.name]
 }
- 
 
+# ----------- GKE CLUSTER -----------------
 
-
-resource "google_compute_network" "vpc_network" {
-  name                   = var.vpc_name
-}
-
-
-resource "google_compute_subnetwork" "private_subnet" {
-  name                      = var.subnet_name
-  region                    = var.region_name
-  network                   = google_compute_network.vpc_network.id
-  ip_cidr_range             = "10.10.0.0/24"
-  private_ip_google_access  = true  
-}
- 
-
-
-
-resource "google_compute_service_attachment" "private_services_connection" {
-  name            = "private-services-connection"
-  region          = var.region_name
-  target_service  = "services/servicenetworking.googleapis.com"  
-
-  
-  connection_preference = "PREFERRED"
-
-  
-  nat_subnets = [
-    data.google_compute_subnetwork.private_subnet.id
-  ]
-
-  
-  enable_proxy_protocol = false
-} 
-
-
-  resource "google_container_cluster" "cluster" {
+resource "google_container_cluster" "cluster" {
   name                     = var.cluster_name
   location                 = var.zone_name
   deletion_protection      = false
   remove_default_node_pool = false
   initial_node_count       = 1
-  network                  = data.google_compute_network.vpc_network.name
-  subnetwork               = data.google_compute_subnetwork.private_subnet.name
+
+  network    = data.google_compute_network.vpc_network.self_link
+  subnetwork = data.google_compute_subnetwork.private_subnet.self_link
 
   private_cluster_config {
     enable_private_nodes    = true
-    enable_private_endpoint = false  
+    enable_private_endpoint = false
   }
 
-  depends_on = [data.google_compute_network.vpc_network, data.google_compute_subnetwork.private_subnet]
-} 
+  depends_on = [
+    data.google_compute_network.vpc_network,
+    data.google_compute_subnetwork.private_subnet
+  ]
+}
+
+# ----------- CLOUD SQL INSTANCE PRIVATE IP -----------------
 
 resource "google_sql_database_instance" "db_instance" {
   name             = var.db_instance_name
@@ -92,22 +67,21 @@ resource "google_sql_database_instance" "db_instance" {
     }
   }
 
-   depends_on = [
+  depends_on = [
     google_service_networking_connection.private_service_connect
-  ] 
+  ]
 }
 
+# ----------- SECRET MANAGER -----------------
 
 data "google_secret_manager_secret_version" "db_password" {
-  secret  = "db-password"
+  secret = "db-password"
 }
-
 
 resource "google_sql_database" "database" {
   name     = var.db_name
   instance = google_sql_database_instance.db_instance.name
 }
-
 
 resource "google_sql_user" "db_user" {
   name     = var.db_user
@@ -115,18 +89,19 @@ resource "google_sql_user" "db_user" {
   password = data.google_secret_manager_secret_version.db_password.secret_data
 }
 
- resource "google_compute_firewall" "allow_internal" {
+# ----------- FIREWALL -----------------
+
+resource "google_compute_firewall" "allow_internal" {
   name       = var.firewall_name
   network    = data.google_compute_network.vpc_network.name
   direction  = "INGRESS"
   priority   = 1000
+
   allow {
     protocol = "tcp"
     ports    = ["5432"]
   }
 
-  source_ranges = ["10.10.0.0/24"]  
+  source_ranges = ["10.10.0.0/24"]
   target_tags   = ["gke-node"]
-} 
-
-
+}
