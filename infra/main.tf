20b87058082d647f58be03d11142bf95cb9e2a9e
jahnavi200431 <166016027+jahnavi200431@users.noreply.github.com>
2025-11-24 16:28:34 +0530

    Update main.tf

--- infra/main.tf
+++ infra/main.tf
@@ -1,6 +1,19 @@
 # ---------------------------
 # PROVIDER
 # ---------------------------
+terraform {
+  required_providers {
+    google = {
+      source  = "hashicorp/google"
+      version = ">= 5.0"
+    }
+    google-beta = {
+      source  = "hashicorp/google-beta"
+      version = ">= 5.0"
+    }
+  }
+}
+
 provider "google-beta" {
   project = var.project_id
   region  = var.region_name
